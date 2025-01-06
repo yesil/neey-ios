99ca2c1be8a872d8f36739e813e879397a7c6895
Ilyas Türkben <ilyas@adobe.com>
2025-01-06 15:30:13 +0100

    Initial Commit

---
+++ Neey/NeeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeeyApp.swift
+//  Neey
+//
+//  Created by Ilyas Stephane Turkben on 06.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct NeeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
